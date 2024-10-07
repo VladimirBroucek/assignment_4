3370279bce8f8ae1aa4bbcde3a94184a155d2253
Vladimír Brouček <broukla987@gmail.com>
2024-10-06 19:40:45 -0700

    assign 4

---
+++ public/js/firebase.js
@@ -0,0 +1,23 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDastaK3xGBIYlj1DYrd09mq0-H_rQri1w",
+    authDomain: "portfoliov1vladimirbroucek.firebaseapp.com",
+    projectId: "portfoliov1vladimirbroucek",
+    storageBucket: "portfoliov1vladimirbroucek.appspot.com",
+    messagingSenderId: "924833485045",
+    appId: "1:924833485045:web:e4289a4eac720e72c5f5bc",
+    measurementId: "G-55SGTQSD0P"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
